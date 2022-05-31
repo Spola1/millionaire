c666eb129724115af5adedb40b0284a436ad8b6e
Spola1 <1111.22224@mail.ru>
2022-05-31 18:42:20 +0300

    show.html.erb_spec.rb

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'users/show', type: :view do
+  let(:user) { create(:user, id: 1, email: '1@1.ru', name: 'Валера') }
+  before do
+    assign(:user, user)
+    assign(:games, [build_stubbed(:game, id: 1), build_stubbed(:game, id: 2)])
+  end
+
+  it 'render nickname' do
+    render
+    expect(rendered).to match 'Валера'
+  end
+
+  it 'render game' do
+    stub_template 'users/_game.html.erb' => "<%= game.id %><br/>"
+    render
+    expect(rendered).to match /1.*2/m
+  end
+
+  context 'when current user == user' do
+    before do
+      sign_in user
+      render
+    end
+
+    it 'render edit profile button' do
+      expect(rendered).to match('Сменить имя и пароль')
+    end
+  end
+
+  context 'when current user != user' do
+    it 'does not render edit profile button for guest' do
+      render
+      expect(rendered).not_to match('Сменить имя и пароль')
+    end
+
+    it 'does not render edit profile button for another user' do
+      sign_in create(:user, id: 2, email: '2@2.ru', name: 'Петя')
+      render
+
+      expect(rendered).not_to match('Сменить имя и пароль')
+    end
+  end
+end
